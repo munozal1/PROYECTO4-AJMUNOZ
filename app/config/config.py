125949755933b4a111152571276ae4243e924115
munozal1 <alvarojesus_munoz@sierracol.com.co>
2025-04-27 00:22:52 -0500

    se actualiza el mysql database uri

--- app/config/config.py
+++ app/config/config.py
@@ -8,4 +8,5 @@ class Config():
     #LOCAL
     #SQLALCHEMY_DATABASE_URI=f"mysql+pymysql://{os.getenv('MYSQL_PYTHON_USER')}:{os.getenv('MYSQL_PYTHON_PWD')}@localhost:3306/heladeria?charset=utf8mb4"
     #RAILWAY
-    SQLALCHEMY_DATABASE_URI=f"mysql+pymysql://{os.getenv('MYSQL_PYTHON_USER')}:{os.getenv('MYSQL_PYTHON_PWD')}@shuttle.proxy.rlwy.net:58957/railway"
+    #SQLALCHEMY_DATABASE_URI=f"mysql+pymysql://{os.getenv('MYSQL_PYTHON_USER')}:{os.getenv('MYSQL_PYTHON_PWD')}@shuttle.proxy.rlwy.net:58957/railway"
+    SQLALCHEMY_DATABASE_URI="mysql+pymysql://root:SPdYrXRZBvYRvAYAASPrrqbUwovNlTTZ@shuttle.proxy.rlwy.net:58957/railway"
